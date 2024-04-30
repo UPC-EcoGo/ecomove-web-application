a428fa9206e1aae5093ca1abcccc961409cb10f7
MarianoAmes <129983231+MarianoAmes@users.noreply.github.com>
2024-04-30 00:31:50 -0500

    feat: add navbar component

--- src/app/public/components/navbar/navbar.component.ts
+++ src/app/public/components/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIcon } from '@angular/material/icon';
-
-
+import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatIcon ],
+  imports: [MatToolbarModule, MatIcon, RouterModule, RouterLink ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
