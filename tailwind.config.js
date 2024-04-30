cd82f47c2530bad47bb6e132202f1e1634c464c0
MarianoAmes <129983231+MarianoAmes@users.noreply.github.com>
2024-04-30 00:30:28 -0500

    feat: add background color in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'nav-blue': '#55889D'
+      }
+    },
   },
   plugins: [],
 }
