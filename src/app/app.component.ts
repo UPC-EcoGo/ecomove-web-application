3c95549b4472c99e95fd80d0d0ddb31d4b40e121
Angello Sosa Colca <angellososaa2205@gmail.com>
2024-05-01 17:43:47 -0500

    feat: add styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './public/components/navbar/navbar.component';
+import {PaymentMethodsComponent} from "./payment/components/payment-methods/payment-methods.component";
+import {FooterComponent} from "./public/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, PaymentMethodsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+colors:{
+        'nav-blue': '#55889D',
+        'page': '#E6F4FB',
+        'content1': '#C9E5F1',
+        'content2': '#ACD5E6'
+      }
+    },
   },
   plugins: [],
 }
