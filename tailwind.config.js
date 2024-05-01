72d01457ccfc878704ea9bf56c193e1a4c119c84
MarianoAmes <129983231+MarianoAmes@users.noreply.github.com>
2024-05-01 12:41:28 -0500

    feat: add styles for reservation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'background-blue': '#E6F4FB',
+        'background-reservation' : '#C9E5F1',
+        'background-input': '#ACD5E6',
+        'button-color':'#55889D'
+      },
+      fontFamily:{
+        'kumbh-sans': 'Kumbh Sans'
+      }
+    },
   },
   plugins: [],
 }
