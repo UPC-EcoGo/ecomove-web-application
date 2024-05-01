379023c21482484d785463541a82572e8ff6f19f
MarianoAmes <129983231+MarianoAmes@users.noreply.github.com>
2024-05-01 01:52:22 -0500

    feat: add styles for background and cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'card-text': '#ACD5E6',
+        'background-blue': '#E6F4FB'
+      },
+      fontFamily:{
+        'kumbh-sans': 'Kumbh Sans'
+      }
+    },
   },
   plugins: [],
 }
