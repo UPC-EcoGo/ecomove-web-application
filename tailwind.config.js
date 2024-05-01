a6d00b576be8eed69308b231c211b523b387c8a5
MarianoAmes <129983231+MarianoAmes@users.noreply.github.com>
2024-05-01 10:24:20 -0500

    feat: add styles for buttons in settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
     extend: {
       colors:{
         'card-text': '#ACD5E6',
-        'background-blue': '#E6F4FB'
+        'background-blue': '#E6F4FB',
+        'settings-background': '#C9E5F1'
       },
       fontFamily:{
         'kumbh-sans': 'Kumbh Sans'
