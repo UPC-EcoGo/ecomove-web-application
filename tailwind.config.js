9052bac0c07d382f385f6f603de499351e55650c
Christopher <79271081+ChrisByBits@users.noreply.github.com>
2024-05-02 18:36:52 -0500

    feat: fix theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors:{
-        'nav-blue': '#55889D'
+      colors: {
+        'primary': {
+          100: '#ffffff',
+          200: '#e6f4fb',
+          300: '#d8edf6',
+          400: '#c9e5f1',
+          500: '#acd5e6',
+          600: '#8fc6dc',
+          700: '#71b6d1',
+          800: '#55889d'
+        },
       }
     },
   },
