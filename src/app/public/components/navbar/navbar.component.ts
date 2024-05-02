cb039fee49a20a5a5e86b331c11084479a065de3
Christopher <79271081+ChrisByBits@users.noreply.github.com>
2024-05-02 18:39:27 -0500

    feat: make navbar and footer responsive

--- src/app/public/components/navbar/navbar.component.ts
+++ src/app/public/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIcon } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
 import { RouterLink } from '@angular/router';
+
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatIcon, RouterModule, RouterLink ],
+  imports: [MatToolbarModule, MatIcon, RouterLink ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
