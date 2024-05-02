e079d7e1280801804eebbe0527e5785571496176
Angello Sosa Colca <angellososaa2205@gmail.com>
2024-05-02 05:15:53 -0500

    feat: fix settings page

--- src/app/user-management/components/settings-page/settings-page.component.ts
+++ src/app/user-management/components/settings-page/settings-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-settings-page',
   standalone: true,
-  imports: [MatIcon, RouterLink],
+  imports: [MatIcon, RouterLink, MatButton],
   templateUrl: './settings-page.component.html',
   styleUrl: './settings-page.component.css'
 })
