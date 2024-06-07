9487ab7017eec0c5e426185ae002a30b6a17ae97
Mily27 <milyta2703@gmail.com>
2024-06-07 13:17:41 -0500

    feat:update pages

--- src/app/payment/pages/methods-page/methods-page.component.ts
+++ src/app/payment/pages/methods-page/methods-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {MatCard, MatCardContent, MatCardImage} from "@angular/material/card";
 import {NavbarComponent} from "../../../public/components/navbar/navbar.component";
+import {FooterComponent} from "../../../public/components/footer/footer.component";
 
 @Component({
   selector: 'app-methods-page',
@@ -9,8 +10,9 @@ import {NavbarComponent} from "../../../public/components/navbar/navbar.componen
     MatCard,
     MatCardContent,
     MatCardImage,
-    NavbarComponent
-],
+    NavbarComponent,
+    FooterComponent
+  ],
   templateUrl: './methods-page.component.html',
   styleUrl: './methods-page.component.css'
 })
